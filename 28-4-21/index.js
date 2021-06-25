d24cf8427285520a16a01734112325fed0e7379d
1NT18IS114 <82773608+1NT18IS114@users.noreply.github.com>
2021-06-25 10:24:15 +0530

    Update index.js

--- 28-4-21/index.js
+++ 28-4-21/index.js
@@ -1,20 +1,15 @@
-<!-- Title : To demonstrate fetch() function -->
-<!-- Author: Pranjal Prateek -->
-<!-- Date:28-04-2021 -->
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Fetch Data form API</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-</head>
-<body>
-    <h1>Assignment 3</h1>
-    <h2>This program fetches an activity from boredapi and displays the activity on screen</h2>
-    <button type="button" class="btn btn-danger" id="fetch">ClickME</button>
-    <p id="gettingdata"></p>
-    <script src="index.js" defer></script>
-</body>
-</html>
+let getButton=document.getElementById('fetch');
+
+const fetchData = () =>{
+    fetch('https://www.boredapi.com/api/activity')
+    .then(res => res.json())
+    .then( active => {
+        let result=active.activity;
+        console.log(active)
+        document.getElementById("gettingdata").innerHTML=result
+    })
+    .then(res => console.log(res))
+    .catch(error=>console.log(error))
+}
+
+getButton.addEventListener('click',fetchData);
